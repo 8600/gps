e31c2063b28c92dfba6011e078e84d55b771e766
PUGE <100284685@qq.com>
2018-11-01 20:49:44 +0800

    save

--- main.py
+++ main.py
@@ -2,52 +2,39 @@
 # -*- coding: UTF-8 -*-
 import requests
 import json
+import MySQLdb
 from flask import Flask, request
 app = Flask(__name__)
 
-# 截取字符串
-def subString(str, start, end, startInd = 0):
-  startIndex = str.find(start, startInd)
-  if (startIndex == -1): return ''
-  # print(startIndex)
-  endIndex = str.find(end, startIndex + 1)
-  if (endIndex == -1): return ''
-  # print(endIndex)
-  # print(str[startIndex + len(start):endIndex])
-  return str[startIndex + len(start):endIndex]
+# 打开数据库连接
+db = MySQLdb.connect("192.168.0.226", "location", "root", "GwlF97#6", charset='utf8' )
 
-# 截取字符串组
-def subStringArr(str, start, end):
-  arr = []
-  nextIndex = 0
-  while True:
-    nextIndex = str.find(start.decode('utf-8'), nextIndex + 1)
-    if nextIndex == -1:
-      return arr
-    temp = subString(str, start, end, nextIndex)
-    if str == '':
-      return arr
-    arr.append(temp)
+# 使用cursor()方法获取操作游标 
+cursor = db.cursor()
 
+# 使用execute方法执行SQL语句
+cursor.execute("SELECT VERSION()")
+
+# 使用 fetchone() 方法获取一条数据
+data = cursor.fetchone()
+
+print "Database version : %s " % data
+
+# 关闭数据库连接
+db.close()
 
 @app.route('/gps/<ip>')
 def index(ip):
+  print(ip)
   if (ip):
     # 没有headers会返回页面不存在
     headers = {
       "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36'
     }
-    res = requests.get('http://www.gpsspg.com/ip/?q=' + ip , headers = headers)
+    res = requests.get('https://www.maxmind.com/geoip/v2.1/city/'+ ip +'?use-downloadable-db=1&demo=1' , headers = headers)
     res.encoding = 'utf-8'
-    # print(res.text)
-    gps = subString(res.text, 'target="_blank">', "</a>").split(",")
-    returnData = {
-      "ip": subString(res.text, '<span class="fcr">', "</span>"),
-      "position": subString(res.text, '<span class="fcg">', "</span><br />"),
-      "longitude": gps[0],
-      "latitude": gps[1]
-    }
-    return json.dumps(returnData)
+    print(res.json())
+    return res.text
   return ''
   
 
